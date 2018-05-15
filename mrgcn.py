aae485c290bb94fa5124ca4e8dbd8c2f837e1cb9
Xander Wilcke <w.x.wilcke@vu.nl>
2018-05-15 13:14:12 +0200

    Added k-fold xval support

--- mrgcn.py
+++ mrgcn.py
@@ -2,7 +2,6 @@
 
 import logging
 import argparse
-from os.path import splitext
 from time import time
 
 import toml
@@ -13,7 +12,153 @@ from data.io.tsv import TSV
 from data.utils import is_readable, is_writable
 from embeddings import graph_structure
 from tasks.node_classification import build_dataset, build_model, evaluate_model
-from tasks.utils import create_splits, sample_mask, set_seed, strip_graph
+from tasks.utils import mksplits, init_fold, mkfolds, sample_mask, set_seed, strip_graph
+
+
+def single_run(A, X, Y, X_node_map, tsv_writer, config):
+    tsv_writer.writerow(["epoch", "training_loss", "training_accurary",
+                                  "validation_loss", "validation_accuracy",
+                                  "test_loss", "test_accuracy"])
+
+    # create splits
+    dataset = mksplits(X, Y, X_node_map, 
+                            config['task']['dataset_ratio'])
+
+    # compile model computation graph
+    model = build_model(X, Y, A, config)
+    
+    # train model
+    nepoch = config['model']['epoch']
+    batch_size = X.shape[0]  # number of nodes
+    sample_weights = sample_mask(dataset['train']['X_idx'],
+                                            Y.shape[0])
+
+    for epoch in train_model(A, model, dataset, sample_weights, batch_size, nepoch):
+        # log metrics
+        tsv_writer.writerow([str(epoch[0]), 
+                             str(epoch[1]),
+                             str(epoch[2]),
+                             str(epoch[3]), 
+                             str(epoch[4]),
+                             "-1", "-1"])
+
+    # test model
+    test_loss, test_acc = test_model(A, model, dataset, batch_size)
+    # log metrics
+    tsv_writer.writerow(["-1", "-1", "-1", "-1", "-1", 
+                         str(test_loss[0]), str(test_acc[0])])
+
+    return (test_loss[0], test_acc[0])
+
+def kfold_crossvalidation(A, X, Y, X_node_map, k, tsv_writer, config):
+    tsv_writer.writerow(["fold", "epoch", 
+                         "training_loss", "training_accurary",
+                         "validation_loss", "validation_accuracy",
+                         "test_loss", "test_accuracy"])
+
+    # generate fold indices
+    folds_idx = mkfolds(X_node_map.shape[0], k)
+
+    results = []
+    logger.info("Starting {}-fold cross validation".format(k))
+    for fold in range(1, k+1):
+        logger.info("Fold {} / {}".format(fold, k))
+
+        # compile model computation graph
+        model = build_model(X, Y, A, config)
+        
+        # initialize fold 
+        dataset = init_fold(X, Y, X_node_map, folds_idx[fold-1],
+                            config['task']['dataset_ratio'])
+
+        # train model
+        nepoch = config['model']['epoch']
+        batch_size = X.shape[0]  # number of nodes
+        sample_weights = sample_mask(dataset['train']['X_idx'],
+                                                Y.shape[0])
+
+        for epoch in train_model(A, model, dataset, sample_weights, batch_size, nepoch):
+            # log metrics
+            tsv_writer.writerow([str(fold),
+                                 str(epoch[0]), 
+                                 str(epoch[1]),
+                                 str(epoch[2]),
+                                 str(epoch[3]), 
+                                 str(epoch[4]),
+                                 "-1", "-1"])
+
+        # test model
+        test_loss, test_acc = test_model(A, model, dataset, batch_size)
+        results.append((test_loss[0], test_acc[0]))
+
+        # log metrics
+        tsv_writer.writerow([str(fold), 
+                             "-1", "-1", "-1", "-1", "-1", 
+                             str(test_loss[0]), str(test_acc[0])])
+
+    mean_loss, mean_acc = tuple(sum(e)/len(e) for e in zip(*results))
+    tsv_writer.writerow(["-1", "-1", "-1", "-1", "-1", "-1", 
+                         str(mean_loss), str(mean_acc)])
+
+    return (mean_loss, mean_acc)
+
+def train_model(A, model, dataset, sample_weights, batch_size, nepoch):
+    logging.info("Training for {} epoch".format(nepoch))
+    # Log wall-clock time
+    t0 = time()
+    for epoch in range(1, nepoch+1):
+        # Single training iteration
+        model.fit(x=[dataset['train']['X']] + A, 
+                  y=dataset['train']['Y'],
+                  batch_size=batch_size,
+                  nb_epoch=1,
+                  shuffle=False,
+                  sample_weight=sample_weights,
+                  validation_data=([dataset['val']['X']] + A, 
+                                    dataset['val']['Y']),
+                  callbacks=[],
+                  verbose=0)
+
+        # Predict on full dataset
+        Y_hat = model.predict(x=[dataset['train']['X']] + A, 
+                              batch_size=batch_size,
+                              verbose=0)
+
+        # Train / validation scores
+        train_val_loss, train_val_acc = evaluate_model(Y_hat, 
+                                                       [dataset['train']['Y'],
+                                                        dataset['val']['Y']],
+                                                       [dataset['train']['X_idx'],
+                                                        dataset['val']['X_idx']])
+    
+        logging.info("{:04d} ".format(epoch) \
+                     + "| train loss {:.4f} / acc {:.4f} ".format(train_val_loss[0],
+                                                                  train_val_acc[0])
+                     + "| val loss {:.4f} / acc {:.4f}".format(train_val_loss[1],
+                                                               train_val_acc[1]))
+
+        yield (epoch,
+               train_val_loss[0], train_val_acc[0], 
+               train_val_loss[1], train_val_acc[1])
+    
+    logging.info("training time: {:.2f}s".format(time()-t0))
+
+def test_model(A, model, dataset, batch_size):
+    # Predict on full dataset
+    Y_hat = model.predict(x=[dataset['train']['X']] + A, 
+                          batch_size=batch_size,
+                          verbose=0)
+
+    test_loss, test_acc = evaluate_model(Y_hat, 
+                                         [dataset['test']['Y']],
+                                         [dataset['test']['X_idx']])
+    
+    logging.info("Performance on test set: loss {:.4f} / accuracy {:.4f}".format(
+                  test_loss[0],
+                  test_acc[0]))
+
+    return (test_loss, test_acc)
+
 
 def run(args, tsv_writer, config):
     set_seed(config['task']['seed'])
@@ -33,102 +178,32 @@ def run(args, tsv_writer, config):
             X = tb.get('X')
             Y = tb.get('Y')
             X_node_map = tb.get('X_node_map')
-
-    # compile model computation graph
-    model = build_model(X, Y, A, config)
-
-    # create splits
-    dataset = create_splits(X, Y, X_node_map, config['task']['dataset_ratio'])
-
-    # train model
-    nepochs = config['model']['epoch']
-    batch_size = X.shape[0]  # number of nodes
-    sample_weights = sample_mask(dataset['train']['X_idx'],
-                                            Y.shape[0])
-
-    logging.info("Training for {} epoch".format(nepochs))
-    tsv_writer.writerow(["epoch", "training_loss", "training_accurary",
-                                  "validation_loss", "validation_accuracy",
-                                  "test_loss", "test_accuracy"])
-    # Log wall-clock time
-    t0 = time()
-    for epoch in range(1, nepochs+1):
-        # Single training iteration
-        model.fit(x=[dataset['train']['X']] + A, 
-                  y=dataset['train']['Y'],
-                  batch_size=batch_size,
-                  nb_epoch=1,
-                  shuffle=False,
-                  sample_weight=sample_weights,
-                  validation_data=([dataset['val']['X']] + A, 
-                                   dataset['val']['Y']),
-                  callbacks=[],
-                  verbose=0)
-
-        # Predict on full dataset
-        Y_hat = model.predict(x=[dataset['train']['X']] + A, 
-                              batch_size=batch_size,
-                              verbose=0)
-
-        # Train / validation scores
-        train_val_loss, train_val_acc = evaluate_model(Y_hat, 
-                                                       [dataset['train']['Y'],
-                                                        dataset['val']['Y']],
-                                                       [dataset['train']['X_idx'],
-                                                        dataset['val']['X_idx']])
-
-        # log metrics
-        tsv_writer.writerow([str(epoch), 
-                             str(train_val_loss[0]),
-                             str(train_val_acc[0]),
-                             str(train_val_loss[1]), 
-                             str(train_val_acc[1]),
-                             "-1", "-1"])
-
-        logging.info("{:04d} ".format(epoch) \
-                     + "| train loss {:.4f} / acc {:.4f} ".format(train_val_loss[0],
-                                                                  train_val_acc[0])
-                     + "| val loss {:.4f} / acc {:.4f}".format(train_val_loss[1],
-                                                               train_val_acc[1]))
-
-    logging.info("training time (s): {:.2f}".format(time()-t0))
-
-    # Testing
-    test_loss, test_acc = evaluate_model(Y_hat, 
-                                         [dataset['test']['Y']],
-                                         [dataset['test']['X_idx']])
-
-    # log metrics
-    tsv_writer.writerow(["-1", "-1", "-1", "-1", "-1", 
-                         str(test_loss[0]), str(test_acc[0])])
-
-    logging.info("Performance on test set: loss {:.4f} / accuracy {:.4f}".format(
-                  test_loss[0],
-                  test_acc[0]))
-    if args.verbose < 1:
-        print("Performance on test set: loss {:.4f} / accuracy {:.4f}".format(
-                  test_loss[0],
-                  test_acc[0]))
-
-    return model
-
-def set_logging(args, timestamp):
-    log_path = args.log_directory
-    if not is_writable(log_path):
-        return
-
-    filename = "{}{}.log".format(log_path, timestamp) if log_path.endswith("/") \
-                    else "{}/{}.log".format(log_path, timestamp)
     
+    if config['task']['kfolds'] < 0:
+        loss, accuracy = single_run(A, X, Y, X_node_map, tsv_writer, config)
+    else:
+        loss, accuracy = kfold_crossvalidation(A, X, Y, X_node_map,
+                                               config['task']['kfolds'],
+                                               tsv_writer, config)
+
+    logging.info("Mean performance: loss {:.4f} / accuracy {:.4f}".format(
+                  loss,
+                  accuracy))
+    if args.verbose < 1:
+        print("Mean performance: loss {:.4f} / accuracy {:.4f}".format(
+                  loss,
+                  accuracy))
+
+def init_logger(filename, verbose=0):
     logging.basicConfig(filename=filename,
                         format='[%(asctime)s] %(module)s/%(funcName)s | %(levelname)s: %(message)s',
                         level=logging.DEBUG)
 
-    if args.verbose > 0:
+    if verbose > 0:
         stream_handler = logging.StreamHandler()
 
         level = logging.INFO
-        if args.verbose >= 2:
+        if verbose >= 2:
             level = logging.DEBUG
         stream_handler.setLevel(level)
         
@@ -139,38 +214,32 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument("-c", "--config", help="Configuration file (toml)", required=True, default=None)
-    parser.add_argument("-i", "--input", help="Prepared input file (tar)", default=None)
-    parser.add_argument("-m", "--mode", help="Train or test a model", choices=("train", "test"), default="train")
-    parser.add_argument("-o", "--output", help="Output file (tsv)", default=None)
+    parser.add_argument("-i", "--input", help="Optional prepared input file (tar)", default=None)
+    parser.add_argument("-o", "--output", help="Output directory", default="/tmp/")
     parser.add_argument("-v", "--verbose", help="Increase output verbosity", action='count', default=0)
-    parser.add_argument("--log_directory", help="Where to save the log file", default="../log/")
-    parser.add_argument("--save_model", help="Save the trained model to file (h5)", action="store_true")
     args = parser.parse_args()
 
+    # load configuration
     assert is_readable(args.config)
     config = toml.load(args.config)
 
-    if args.output is None:
-        args.output = './' + config['name'] + '{}.tsv'.format(timestamp)
-    assert is_writable(args.output)
-    tsv_writer = TSV(args.output, 'w')
+    # set output base filename
+    baseFilename = "{}{}{}".format(args.output, config['name'], timestamp) if args.output.endswith("/") \
+                    else "{}/{}{}".format(args.output, config['name'], timestamp)
+    assert is_writable(baseFilename)
 
-    set_logging(args, timestamp)
+    init_logger(baseFilename+'.log', args.verbose)
     logger = logging.getLogger(__name__)
+    
+    tsv_writer = TSV(baseFilename+'.tsv', 'w')
+
+    # log parameters
     logger.debug("Arguments:\n{}".format(
         "\n".join(["\t{}: {}".format(arg, getattr(args, arg)) for arg in vars(args)])))
     logger.debug("Configuration:\n{}".format(
         "\n".join(["\t{}: {}".format(k,v) for k,v in config.items()])))
 
-    # write results
-    model = run(args, tsv_writer, config)
-    
-    if args.save_model is True:
-        raise NotImplementedError()
-        output = splitext(args.output)[1] + '.h5'
-        assert is_writable(output)
-        
-        logger.info("Saving trained model to {}".format(output))
-        model.save_model(output)
+    # run training
+    run(args, tsv_writer, config)
     
     logging.shutdown()
--- tasks/utils.py
+++ tasks/utils.py
@@ -41,7 +41,56 @@ def strip_graph(knowledge_graph, config):
 
     return target_triples
 
-def create_splits(X, Y, X_nodes_map, dataset_ratio=(.6,.2,.2), shuffle=True):
+def mkfolds(n, k, shuffle=True):
+    assert k > 2
+    logger.debug("Generating {} folds".format(k))
+
+    # indices of targets
+    idx = np.array(range(n))
+    if shuffle:
+        logger.debug("Shuffling dataset")
+        np.random.shuffle(idx)
+
+    fold_size = int(len(idx)/k)
+
+    folds = []
+    for i in range(k):
+        test_idx = idx[fold_size*i:fold_size*(i+1)]
+        train_idx = np.setdiff1d(idx, test_idx)  # takes remainder
+        folds.append({'train':train_idx, 'test':test_idx})
+
+    return folds
+
+def init_fold(X, Y, X_nodes_map, idx_dict, dataset_ratio=(.7,.2,.1)):
+    assert round(sum(dataset_ratio), 2) == 1.0
+    logger.debug("Initializing fold")
+
+    # calculate validation split
+    val_split = dataset_ratio[-1]
+    val_idx = int(len(idx_dict['train'])*val_split)
+
+    # split datasets
+    X_train_idx = X_nodes_map[idx_dict['train'][:-val_idx]]
+    X_test_idx = X_nodes_map[idx_dict['test']]
+    X_val_idx = X_nodes_map[idx_dict['train'][-val_idx:]]
+ 
+    # split Y
+    Y_train = np.zeros(Y.shape)
+    Y_test = np.zeros(Y.shape)
+    Y_val = np.zeros(Y.shape)
+
+    Y_train[X_train_idx] = np.array(Y[X_train_idx].todense())
+    Y_test[X_test_idx] = np.array(Y[X_test_idx].todense())
+    Y_val[X_val_idx] = np.array(Y[X_val_idx].todense())
+
+    # X stays unmodified during featureless learning
+    # X = X
+
+    return { 'train': { 'X': X, 'Y': Y_train, 'X_idx': X_train_idx },
+            'test': { 'X': X, 'Y': Y_test, 'X_idx': X_test_idx },
+            'val': { 'X': X, 'Y': Y_val, 'X_idx': X_val_idx }}
+
+def mksplits(X, Y, X_nodes_map, dataset_ratio=(.7,.2,.1), shuffle=True):
     assert round(sum(dataset_ratio), 2) == 1.0
     logger.debug("Creating train-test-validation sets with ratio {}".format(dataset_ratio))
     # indices of targets
