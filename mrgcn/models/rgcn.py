fixed double softmax; added argument to write ranks to disk

mrgcn/models/rgcn.py
@@ -72,8 +72,8 @@ class RGCN(nn.Module):
                                p=self.p_dropout)
                 X = torch.mul(X.T, ones).T
 
-
-            X = f_activation(X)
+            if f_activation is not None:
+                X = f_activation(X)
 
         return X
 

mrgcn/run.py
@@ -5,6 +5,7 @@ import argparse
 from os import getpid
 from time import time
 
+import numpy as np
 import toml
 import torch
 
@@ -36,7 +37,7 @@ def run(A, X, Y, C, data, acc_writer, device, config,
                                                featureless, test_split)
         return (mrr, hits_at_k)
 
-def main(args, acc_writer, out_writer, config):
+def main(args, acc_writer, out_writer, baseFilename, config):
     set_seed(config['task']['seed'])
 
     test_split = 'test' if args.test else 'valid' # use test split?
@@ -83,10 +84,13 @@ def main(args, acc_writer, out_writer, config):
         print("loss {:.4f} / accuracy {:.4f}".format(loss, acc))
     elif task == "link prediction":
         rank_type = "filtered" if config['task']['filter_ranks'] else "raw"
-        mrr, hits = out
+        mrr, hits, ranks = out
         print("MRR ({}) {:.4f}".format(rank_type, mrr)
               + " / " + " / ".join(["H@{} {:.4f}".format(k,v) for
                                           k,v in hits.items()]))
+
+        if args.save_ranks:
+            np.save(baseFilename+"_ranks.npy", ranks)
 
 def init_logger(filename, dry_run, verbose=0):
     if dry_run:
@@ -125,6 +129,8 @@ if __name__ == "__main__":
     parser.add_argument("-v", "--verbose", help="Increase output verbosity", action='count', default=0)
     parser.add_argument("--dry_run", help="Suppress writing output files to disk",
                         action='store_true')
+    parser.add_argument("--save_ranks", help="Write final ranks to disk (npy). Link prediction only.",
+                        action='store_true')
     parser.add_argument("--test", help="Report accuracy on test set rather than on validation set",
                         action='store_true')
     args = parser.parse_args()
@@ -153,6 +159,6 @@ if __name__ == "__main__":
         "\n".join(["\t{}: {}".format(k,v) for k,v in config.items()])))
 
     # run training
-    main(args, acc_writer, out_writer, config)
+    main(args, acc_writer, out_writer, baseFilename, config)
 
     logging.shutdown()

mrgcn/tasks/node_classification.py
@@ -216,11 +216,10 @@ def build_model(C, Y, A, modules_config, config, featureless):
         i += 1
 
     # output layer
-    # applies softmax over possible classes
     modules.append((layers[i-1]['hidden_nodes'],
                     Y_dim,
                     layers[i-1]['type'],
-                    nn.Softmax(dim=1)))
+                    None))
 
     model = MRGCN(modules, modules_config, num_relations, num_nodes,
                   num_bases=config['model']['num_bases'],